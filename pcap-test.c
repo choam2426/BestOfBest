0157c976f73e1d58e332f9ed4b8a32c70af2d29d
choam2426 <choam2426@gmail.com>
2023-07-22 14:11:59 +0900

    clear

--- pcap-test.c
+++ pcap-test.c
@@ -1,6 +1,8 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #define ETHER_ADDR_LEN 6
 
 struct libnet_ethernet_hdr
@@ -10,9 +12,99 @@ struct libnet_ethernet_hdr
     u_int16_t ether_type;                 /* protocol */
 };
 
+struct libnet_ipv4_hdr
+{
+    u_int8_t ip_hl:4,      /* header length */
+           ip_v:4;         /* version */
+#if (LIBNET_BIG_ENDIAN)
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
+
+struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       /* source port */
+    u_int16_t th_dport;       /* destination port */
+    u_int32_t th_seq;          /* sequence number */
+    u_int32_t th_ack;          /* acknowledgement number */
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t th_x2:4,         /* (unused) */
+           th_off:4;        /* data offset */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t th_off:4,        /* data offset */
+           th_x2:4;         /* (unused) */
+#endif
+    u_int8_t  th_flags;       /* control flags */
+#ifndef TH_FIN
+#define TH_FIN    0x01      /* finished send data */
+#endif
+#ifndef TH_SYN
+#define TH_SYN    0x02      /* synchronize sequence numbers */
+#endif
+#ifndef TH_RST
+#define TH_RST    0x04      /* reset the connection */
+#endif
+#ifndef TH_PUSH
+#define TH_PUSH   0x08      /* push data to the app layer */
+#endif
+#ifndef TH_ACK
+#define TH_ACK    0x10      /* acknowledge */
+#endif
+#ifndef TH_URG
+#define TH_URG    0x20      /* urgent! */
+#endif
+#ifndef TH_ECE
+#define TH_ECE    0x40
+#endif
+#ifndef TH_CWR   
+#define TH_CWR    0x80
+#endif
+    u_int16_t th_win;         /* window */
+    u_int16_t th_sum;         /* checksum */
+    u_int16_t th_urp;         /* urgent pointer */
+};
+
 void PrintMac(uint8_t *m){
-	printf("%02x:%02x:%02x:%02x:%02x:%02x",  m[0],m[1],m[2],m[3],m[4],m[5]);	
+	printf("MAC Address : %02x:%02x:%02x:%02x:%02x:%02x ",  m[0],m[1],m[2],m[3],m[4],m[5]);	
 }
+
+void PrintIP(struct libnet_ipv4_hdr* ipv4_hdr){
+	printf("SIP Address : %s DIP Address : %s ", inet_ntoa(ipv4_hdr->ip_src), inet_ntoa(ipv4_hdr->ip_dst));
+}
+
+
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
 	printf("sample: pcap-test wlan0\n");
@@ -55,11 +147,18 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
-		printf("%u bytes captured\n", header->caplen);
 		struct libnet_ethernet_hdr *eth_hdr=(struct libnet_ethernet_hdr *)packet;
-		PrintMac(eth_hdr->ether_shost);
+		struct libnet_ipv4_hdr *ipv4_hdr=(struct libnet_ipv4_hdr *)(packet + 14);
+		struct libnet_tcp_hdr *tcp_hdr=(struct libnet_tcp_hdr *)(packet + 14 + ipv4_hdr->ip_hl*4);
+		if (ipv4_hdr->ip_p==6){
+			PrintMac(eth_hdr->ether_shost);
+			PrintIP(ipv4_hdr);
+			printf("%d %d\n", ntohs(tcp_hdr->th_sport), ntohs(tcp_hdr->th_dport));
+		}
 
 	}
+
+
 
 	pcap_close(pcap);
 }
