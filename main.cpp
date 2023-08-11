27e9b0e8f66563b56a1a43741bc302384e0ab7cf
choam2426 <choam2426@gmail.com>
2023-08-12 06:06:25 +0900

    디버깅 용 print 삭제

--- main.cpp
+++ main.cpp
@@ -82,7 +82,6 @@ int process_ip_pair(char* inface, char* sender_ip, char* target_ip) {
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(packet));
 	}
-	fprintf(stderr, "3");
 	while (true) {
 		struct pcap_pkthdr* header;
 		const u_char* re_packet;
@@ -98,7 +97,6 @@ int process_ip_pair(char* inface, char* sender_ip, char* target_ip) {
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(packet));
 	}
-	fprintf(stderr, "2");
 	while (true) {
 		struct pcap_pkthdr* header;
 		const u_char* re_packet;
@@ -130,18 +128,14 @@ int process_ip_pair(char* inface, char* sender_ip, char* target_ip) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", atk, pcap_geterr(packet));
 	}	
 
-	
-	fprintf(stderr, "4");
 	while (true) {
 		struct pcap_pkthdr* header;
 		const u_char* re_packet;
 		int res = pcap_next_ex(packet, &header, &re_packet);
-		fprintf(stderr, "8");
 		u_char* relay_packet = new u_char[header->len];
 		memcpy(relay_packet, re_packet, header->len);
 		libnet_ethernet_hdr *eth_hdr=(libnet_ethernet_hdr *)re_packet;
 		libnet_ipv4_hdr *ipv4_hdr=(libnet_ipv4_hdr *)(re_packet + 14);
-		fprintf(stderr, "7");
 		if (ipv4_hdr->ip_dst.s_addr == htonl(Ip(target_ip))){
 			for(int i=0; i<6; i++){
 			relay_packet[i] = target.mac[i];
@@ -157,17 +151,14 @@ int process_ip_pair(char* inface, char* sender_ip, char* target_ip) {
 		
 		}
 		if ((ntohs(eth_hdr->ether_type) == 0x0806)){
-			fprintf(stderr, "asdsasad");
 			EthArpPacket *tmp_eth = (EthArpPacket *)(re_packet);
 			if (tmp_eth->arp_.tip_ == Ip(htonl(Ip(target_ip))) && (tmp_eth->arp_.op_ == htons(ArpHdr::Request))){
-			fprintf(stderr, "========");
 			atk = pcap_sendpacket(packet, reinterpret_cast<const u_char*>(&arp_packet), sizeof(EthArpPacket));
 			if (atk != 0) {
 				fprintf(stderr, "pcap_sendpacket return %d error=%s\n", atk, pcap_geterr(packet));
 			}
 			}
 		}
-		fprintf(stderr, "6");
 	}
 	
 		pcap_close(packet);
