d89acc5872c9ee89d6e40a12f1a384c6d7ff912b
choam2426 <choam2426@gmail.com>
2024-01-23 21:29:35 -0500

    add mac_parser

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include "struct.h"
 #include <pcap.h>
 #include <string>
+#include <array>
+
+
+std::array<uint8_t, 6> mac_parser(std::string string_mac){
+    std::array<uint8_t, 6> byte_mac = {};
+    std::istringstream stream(string_mac);
+    std::string byte_string;
+
+    for (int i = 0; i < 6; ++i) {
+        std::getline(stream, byteString, ':');
+        byte_mac[i] = static_cast<uint8_t>(std::stoi(byteString, nullptr, 16));
+    }
+
+    return byte_mac;
+}
+
 
 void deauth_broadcast(){
-    pass;
+    struct radiotap_header radiotap_header = {};
+    struct deauth_frame deauth_frame = {};
+    
+    
+
 }
 
 void deauth_unicast(){
--- struct.h
+++ struct.h
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <array>
+
 
 #pragma pack(push, 1)
 struct radiotap_header {
@@ -6,5 +8,18 @@ struct radiotap_header {
     uint8_t it_pad = 0;
     uint16_t it_len = 8;
     uint32_t it_present = 0;
-} __attribute__((__packed__));
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct deauth_frame{
+    uint16_t type = 0x000c;
+    uint16_t duration = 0;
+    std::array<uint8_t, 6> d_address;
+    std::array<uint8_t, 6> s_address;
+    std::array<uint8_t, 6> bssid;
+    uint16_t sequence = 0x0000;
+    uint16_t reason_code = 0x0004;
+    uint32_t frame_check = 0;
+};
 #pragma pack(pop)
