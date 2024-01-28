e48b50a5202b9c37634ade4106c32a7403cca684
choam2426 <choam2426@gmail.com>
2024-01-28 17:17:56 +0900

    conntrack 로그 및 검색 기능 완료

--- routers/logs.py
+++ routers/logs.py
@@ -1,5 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+
+from src.conntrack_parser import get_session_table
 
 router = APIRouter(prefix="/logs")
 templates = Jinja2Templates(directory="templates")
+
+
+@router.get(path="/conntrack")
+async def get_conntrack_log_page(request: Request):
+    conntrack_log = get_session_table()
+
+    return templates.TemplateResponse(
+        request=request, name="logs.html", context={"logs": conntrack_log}
+    )
